cb2f20e374ea3695d0864820fa9de9dd25d6c47f
kur <rhdnfka94@gmail.com>
2019-05-23 10:36:47 +0900

    Test cnet model loading

--- test.py
+++ test.py
@@ -147,8 +147,6 @@ if __name__ == "__main__":
     ng.get_gpus(1,False)
     args = parser.parse_args()
 
-    model = InpaintCAModel()
-
     args.imgdir = './dset4paper/tmp/imgs/'
     args.maskdir= './dset4paper/tmp/masks/'
     args.outdir = './dset4paper/tmp/results'
@@ -156,6 +154,54 @@ if __name__ == "__main__":
 
     img_paths = fp.pipe(file_pathseq,human_sorted)(args.imgdir)
     mask_paths = fp.pipe(file_pathseq,human_sorted)(args.maskdir)
+
+    # build inference model
+    sess_config = tf.ConfigProto()                                           
+    sess_config.gpu_options.allow_growth = True                              
+    sess = tf.Session(config=sess_config)                                    
+                                                                             
+    model = InpaintCAModel()                                                 
+    input_image_ph = tf.placeholder(                                         
+        tf.float32, shape = (1,None,None,3)
+    )                                                                    
+    output = model.build_server_graph(input_image_ph)                        
+    output = (output + 1.) * 127.5                                           
+    output = tf.reverse(output, [-1])                                        
+    output = tf.saturate_cast(output, tf.uint8)                              
+
+    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)             
+    assign_ops = []                                                          
+    for var in vars_list:                                                    
+        vname = var.name                                                     
+        from_name = vname                                                    
+        var_value = tf.contrib.framework.load_variable(                      
+            args.checkpoint_dir, from_name
+        )                                                                
+        assign_ops.append(tf.assign(var, var_value))                         
+    sess.run(assign_ops)                                                     
+    print('Model loaded.')                                                   
+    writer = tf.summary.FileWriter('./tmplog')
+    writer.add_graph(sess.graph)
+    writer.flush()
+    writer.close()
+    exit()
+
+    # ---------------------
+    assert image.shape == mask.shape                                     
+                                                                         
+    h, w, _ = image.shape                                                
+    grid = 4                                                             
+    image = image[:h//grid*grid, :w//grid*grid, :]                       
+    mask = mask[:h//grid*grid, :w//grid*grid, :]                         
+    print('Shape of image: {}'.format(image.shape))                      
+                                                                         
+    image = np.expand_dims(image, 0)                                     
+    mask = np.expand_dims(mask, 0)                                       
+    input_image = np.concatenate([image, mask], axis=2)                  
+                                                                         
+    result = sess.run(output, feed_dict={input_image_ph: input_image})   
+    # ---------------------
+
 
     def mk_outpath(srcpath):
         return str(
